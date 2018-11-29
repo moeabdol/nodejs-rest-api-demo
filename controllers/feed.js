4f02f8cdf4c782ff4c5a89156b8eefafcac0422d
moeabdol <mohd.a.saed@gmail.com>
2018-11-29 23:03:06 +0300

    Create basic post route

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,29 @@
 const getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: 'First Post',
+        content: 'This is the first post.'
+      }
+    ]
+  });
+};
+
+const postPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content
+    }
+  });
 };
 
 module.exports = {
-  getPosts
+  getPosts,
+  postPost
 };
